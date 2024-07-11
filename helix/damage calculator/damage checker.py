f483e9fd16d40c2111a4df7eb1729c6bfc10417b
splarsxd <splarsiz34@yahoo.com>
2024-07-11 21:30:16 +0200

    Add files via upload

---
+++ helix/damage calculator/damage checker.py
@@ -0,0 +1,12 @@
+from os import system as sys
+
+value = 2.8
+count = 1
+print(f"Damage | Hits")
+
+while True:
+    if (value * count).is_integer():
+        print(f"{value * count} | {count}")
+        sys("timeout -1 >nul")
+        break
+    count += 1
